95b0f69c6ea5e1658cdfccfc375e3d09661f3428
Cindy Lionis <cindy.lionis@gmail.com>
2024-05-24 15:05:27 +0200

    exercice 9 terminé

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,17 @@
 // votre code ici
 
 export default checkAge;
+
+function checkAge(age) {
+    if (age < 18) {
+        console.log("Vous êtes mineur.");
+    } else if (age <= 65) {
+        console.log("Vous êtes majeur.");
+    } else {
+        console.log("Vous êtes senior.");
+    }
+}
+
+checkAge(15);  
+checkAge(30); 
+checkAge(75); 
