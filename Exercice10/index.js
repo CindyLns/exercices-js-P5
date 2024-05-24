8be44f85d75feb065fb8dc06c7f594f40fb09150
Cindy Lionis <cindy.lionis@gmail.com>
2024-05-24 15:20:56 +0200

    exercice 10 terminé

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,13 @@
 //votre code ici
 
 export default printNumbers
+
+function printNumbers(n) {
+    let result = ""; 
+
+    for (let i = 1; i <= n; i++) {
+        result += i;
+    }
+    return result; 
+}
+console.log(printNumbers(5));
